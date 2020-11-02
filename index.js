2a9f5b13bef76a0e3e38b0915d019a082fa4a86b
Suraj-Gov <surajgovind332000@gmail.com>
2020-11-02 16:30:40 +0530

    init commit, made what i wanted 😁

---
+++ index.js
@@ -0,0 +1,15 @@
+// const img = document.getElementById("unsplash");
+// const width = img.clientWidth;
+// const height = img.clientHeight;
+
+// function calcAspRatioFit(width, height, maxWidth, maxHeight) {
+//   var ratio = Math.min(maxWidth / width, maxHeight / height);
+//   return {
+//     width: width * ratio,
+//     height: height * ratio,
+//   };
+// }
+
+// console.log(calcAspRatioFit(width, height, 1920, 1000));
+
+// it can be done with css only!
